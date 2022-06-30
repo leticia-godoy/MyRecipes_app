65e9d57d82ac789cb4ec0cdbf0ac9c485a4a4131
Letícia Godoy <85085064+leticia-godoy@users.noreply.github.com>
2022-06-30 17:22:30 -0300

    new recipes page was created, chefs icons aswell

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -17,8 +17,32 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_path
     assert_template 'recipes/index'
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+
+  test "should get unique recipe" do
+    get recipe_path(@recipe)
+    assert_template 'template/show'
     assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_match @user.description, response.body
+    assert_match @chef.chefs_name, response.body
+    assert_match @chef.email, response.body
+  end
+
+  test "create new valid recipe" do
+    get new_recipe_path
+  end
+
+  test "reject invalid recipe submissions" do
+    get new_recipe_path
+    assert_template 'recipes/new'
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: { name: " ", description: " " } }
+    end
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
 
 end
