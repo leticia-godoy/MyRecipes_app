3d9cb67a1b3b2b5baa2112b8b37df17ce90d82f6
Letícia Godoy <85085064+leticia-godoy@users.noreply.github.com>
2022-10-04 18:59:59 -0300

    Added: new tables and relationships (ingredients)

---
+++ db/migrate/20221001203113_create_ingredients.rb
@@ -0,0 +1,7 @@
+class CreateIngredients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_03_215213) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_01_210432) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefs_name"
     t.string "email"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_03_215213) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
